bd3cd8b2ca82bf5ecf3f61cf499f775b972522d2
CiceroBrunelli <cicerobrunelli@hotmail.com>
2025-05-24 14:13:25 -0300

    adiciona componente TopBar com template, estilos e testes

---
+++ src/app/comoponents/top-bar/top-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-top-bar',
+  imports: [ButtonModule],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.scss'
+})
+export class TopBarComponent {
+
+}
