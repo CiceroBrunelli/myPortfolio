a9e079248e67e601a899761e725e1208ff39eec7
CiceroBrunelli <cicerobrunelli@hotmail.com>
2025-05-24 14:13:10 -0300

    adiciona componente MyPhoto com template, estilos e testes

---
+++ src/app/comoponents/my-photo/my-photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-photo',
+  imports: [],
+  templateUrl: './my-photo.component.html',
+  styleUrl: './my-photo.component.scss'
+})
+export class MyPhotoComponent {
+
+}
