81b318629ec911d38ea4a4e6029735a343450428
CiceroBrunelli <cicerobrunelli@hotmail.com>
2025-05-24 13:54:34 -0300

    adiciona rotas para MyLinkPage e Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MyLinkPageComponent } from "./pages/my-link-page/my-link-page.component";
+import { HomeComponent } from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MyLinkPageComponent 
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    }
+];
