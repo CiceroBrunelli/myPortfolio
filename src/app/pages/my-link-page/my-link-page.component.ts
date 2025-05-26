a763430b77dc3073491a02235458ba2bb2891606
CiceroBrunelli <cicerobrunelli@hotmail.com>
2025-05-26 20:47:23 -0300

    adiciona componente UserProfile à página MyLinkPage e atualiza estilos

--- src/app/pages/my-link-page/my-link-page.component.ts
+++ src/app/pages/my-link-page/my-link-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TopBarComponent } from "../../components/top-bar/top-bar.component";
+import { UserProfileComponent } from "../../components/user-profile/user-profile.component";
 
 @Component({
   selector: 'app-my-link-page',
-  imports: [TopBarComponent],
+  imports: [TopBarComponent,UserProfileComponent],
   templateUrl: './my-link-page.component.html',
   styleUrl: './my-link-page.component.scss'
 })
