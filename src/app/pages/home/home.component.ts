a8c966a3cbb6e65a72a583da451c29e122d8d2d4
CiceroBrunelli <cicerobrunelli@hotmail.com>
2025-05-31 13:31:42 -0300

    substitui mensagem de 'home works!' pelo componente TopBar no home.component.html e atualiza as importações no HomeComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { TopBarComponent } from "../../components/top-bar/top-bar.component";
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TopBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
