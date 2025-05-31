e5bec495980372cb682961a3003e5c1d3eb58a64
CiceroBrunelli <cicerobrunelli@hotmail.com>
2025-05-31 13:40:00 -0300

    adiciona rota para o componente Portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MyLinkPageComponent } from "./pages/my-link-page/my-link-page.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { PortfolioComponent } from "./pages/portfolio/portfolio.component";
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent
+    },
+    {
+        path: 'portfolio',
+        component: PortfolioComponent
     }
 ];
