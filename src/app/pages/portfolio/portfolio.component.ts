837e5855df51c90c506ae42aee173dc34c4ef948
CiceroBrunelli <cicerobrunelli@hotmail.com>
2025-05-31 13:40:17 -0300

    adiciona componente Portfolio com template e estilos

---
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TopBarComponent } from "../../components/top-bar/top-bar.component";
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [TopBarComponent],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.scss'
+})
+export class PortfolioComponent {
+
+}
